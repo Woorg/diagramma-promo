d0a68cfc3620b7f77bc52ee22a299fd26c3edb5f
Igor Gorlov <gorlov35@gmail.com>
2018-02-28 03:53:44 +0300

    add index page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,9 @@
 import svg4everybody from 'svg4everybody';
 // import $ from 'jquery';
-import slick from 'slick-carousel';
-import 'magnific-popup';
+// import slick from 'slick-carousel';
+// import 'magnific-popup';
 import mask from "jquery-mask-plugin";
-import printThis from "print-this";
+// import printThis from "print-this";
 
 (function ($) {
 
@@ -26,158 +26,6 @@ import printThis from "print-this";
 	$(function() {
 		svg4everybody();
 
-		$('.nav__list.nav__list_subnav > .nav__link').on('click', function (e) {
-			e.preventDefault();
-		});
-
-		var $carousel = $('.objects__carousel .carousel__list');
-		if ($carousel.length) {
-			$carousel.slick({
-				slidesToShow: 4,
-				responsive: [
-					{
-					breakpoint: 1200,
-						settings: {
-						  slidesToShow: 3
-						}
-					},
-					{
-					breakpoint: 991,
-						settings: {
-						  slidesToShow: 3
-						}
-					},
-					{
-					breakpoint: 768,
-						settings: {
-						  slidesToShow: 2
-						}
-					},
-					{
-					breakpoint: 425,
-						settings: {
-						  slidesToShow: 1
-						}
-					}
-
-				]
-			});
-
-		}
-		var $cardSlider = $('.card__slider');
-		var $cardThumbs = $('.card__thumbs');
-
-
-		$cardSlider.slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: false,
-			fade: true,
-			asNavFor: '.card__thumbs'
-		});
-
-		$cardThumbs.slick({
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			asNavFor: '.card__slider',
-			dots: false,
-			centerMode: false,
-			focusOnSelect: true,
-			mobileFirst: true
-		});
-
-
-		// Tabs
-		var tabsNavLink = $('.tabs__item');
-		var tabsNavLinkActive = 'tabs__item_active';
-		var tab = $('.tabs__tab');
-		var tabActive = 'tabs__tab_active';
-
-		tabsNavLink.click(function (event) {
-			event.preventDefault();
-			$(this).addClass(tabsNavLinkActive);
-			$(this).siblings().removeClass(tabsNavLinkActive);
-			var tabCurrent = $(this).children().attr('href');
-			tab.not(tabCurrent).removeClass(tabActive).hide();
-			$(tabCurrent).fadeIn(50).addClass(tabActive).show();
-			if ($('.tabs__tab_active .team__item').length < 3) {
-				console.log('true');
-				$('.tabs__tab_active .team__list').addClass('team__list_quanted-low');
-			}
-		});
-
-		// Objects-full magnific
-
-		$('.carousel__link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.objects-full__link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.history__image').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.team__image').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.service__sertificate').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-
-		$('.examples__link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-
-		$('.card__slider-link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.objects-full__link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		$('.objects-full__link').magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true
-			}
-		});
-
-		// File input
-
-		$('.form__label_file').on('change', '.form__file-input', function(event) {
-			$(this).parent().parent().siblings('.form__file-text').html(this.value);
-		});
 
 		// mask
 
@@ -189,64 +37,30 @@ import printThis from "print-this";
 			}
 		);
 
-		// Popups
 
-		$('.open-popup').magnificPopup({
-			type:'inline',
-			midClick: true
-		});
 
-		$('.related__button').click(function(e){
-			e.preventDefault();
-			$('.related__item').removeClass('related__item_hidden');
-			$(this).hide();
-		});
+		// Faq
 
-		$('.card__print').on('click', function(e) {
-			e.preventDefault();
-			$('.tablepress').printThis({
-				importCSS: false,
-				loadCSS: false,
-				header: $('.card__title'),
-				removeScripts: true
-			});
-		});
+		let allPanels = $('.faq__list > dd').hide();
+		let fT = $('.faq__list > dd.faq__desc_active').show();
 
-		$('.card__download').on('click', function(e) {
-			e.preventDefault();
-		});
+		$('.faq__list dt').click(function() {
+			$('.faq__list dt').removeClass('faq__term_active');
+			$(this).addClass('faq__term_active');
+			let $this = $(this);
+			let $target =  $this.next();
 
-		$('.card__button').on('click', function () {
-			let $hiddenInput = $('input[name=prod_title]');
-			let $productTitle = $('.card__title').text();
-			$hiddenInput.val($productTitle);
-		});
+			console.log($target);
 
-		$('.nav__trigger').on('click', function () {
-			$(this).toggleClass('nav__trigger_active');
-		});
-
-		// class to first element in filter
-
-		$(document).on('facetwp-loaded', function() {
-			$.each(FWP.facets, function(name, vals) {
-				if (FWP.facets[name].length > 0) {
-					$('.facetwp-facet-' + name).addClass('is-active');
-				}
-				else {
-					$('.facetwp-radio:first-child').addClass('is-passive');
-				}
-			});
-			let catFilter = $('.catalog__nav .facetwp-radio').length;
-			console.log(catFilter);
-			if (catFilter < 2) {
-				$('.catalog__nav').hide();
+			if(!$target.hasClass('faq__desc_active')){
+				allPanels.removeClass('faq__desc_active').slideUp();
+				$target.addClass('faq__desc_active').slideDown();
 			}
+			return false;
+
 		});
 
-		if( $('.dd_last_viewed').length === 0 ) {
-			$('.catalog__in').css( 'padding-bottom', 0 );
-		}
+
 
 
 	});
